57820ea4fdd9b8c68a5d0006ee49d6a9240293a5
Daniel <danwa5@gmail.com>
2013-11-24 00:00:45 -0500

    Configure Redis for production on Heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 BookApp::Application.initialize!
+
+# As per the instructions on https://devcenter.heroku.com/articles/redistogo
+# config.gem 'redis'
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,4 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
+# $redis = Redis.new(:host => 'localhost', :port => 6379)
